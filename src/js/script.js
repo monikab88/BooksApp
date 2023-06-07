c4147df111d4c0ee748c94eb0b833a9c7b577b4b
Monika Bielicz <bieliczm@gmail.com>
2023-06-07 02:01:03 +0200

    init actions

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,13 @@
     containerOf: {
       booksList: '.books-list',
     },
+    book: {
+        image: '.books-list .book__image',
+      }
+    };
+  
+  const classNames = {
+    favorite: 'favorite',
   };
   
   const templates = {
@@ -22,5 +29,21 @@
       menuContainer.appendChild(generatedDOM);
     }
   }
+  const favoriteBooks = [];
+
+  function initActions(){
+
+  const books = document.querySelectorAll(select.book.image);
+
+  for(let book of books){
+    book.addEventListener('dblclick', function(event){
+      event.preventDefault();
+      book.classList.add(classNames.favorite);
+      book = clickedElement.getAttribute('data-id');
+      favoriteBooks.push(targetBook);
+    });
+  }
+}
   renderBooks();
+  initActions();
 }
